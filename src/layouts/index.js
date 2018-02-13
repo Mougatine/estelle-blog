12da6741d22e41f2f23ecbc829b45ca24f34880f
Arthur Douillard <douill_a@epita.fr>
2018-02-13 16:17:34 +0100

    Add wallpaper image.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,10 +6,13 @@ import Navbar from '../components/Navbar';
 import './all.sass';
 import 'font-awesome/css/font-awesome.min.css';
 
+import wallpaper from "../../static/img/food_wallpaper.jpg"
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Blog sur la nutrition" />
     <Navbar />
+    <img src={wallpaper} alt="food wallpaper"/>
     <div>{children()}</div>
   </div>
 );
